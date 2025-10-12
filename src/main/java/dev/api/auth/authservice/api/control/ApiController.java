97c6e76083056cbf9a60436239d5001cf27df47a
Asrlex <alexsruc@hotmail.com>
2025-10-12 22:18:02 +0200

    swagger

--- src/main/java/dev/api/auth/authservice/api/control/ApiController.java
+++ src/main/java/dev/api/auth/authservice/api/control/ApiController.java
@@ -1,5 +1,7 @@
 package dev.api.auth.authservice.api.control;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,16 @@ public class ApiController {
 	}
 
 	@GetMapping("/health")
+	@Operation(summary = "Health Check", description = "Check the health status of the application")
+	@io.swagger.v3.oas.annotations.parameters.RequestBody(
+			description = "Health check payload",
+			required = true
+	)
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Application is healthy"),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "Unauthorized access"),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Forbidden access")
+	})
 	public String healthCheck() {
 		return this.apiService.healthCheck();
 	}
